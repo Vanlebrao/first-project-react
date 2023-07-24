69b2d8b02e735a6532012bfc01a0ba8be21756e9
Vanessa Lebrão <vanlebrao@gmail.com>
2023-07-24 15:42:30 -0300

    Upload projeto fullstack react e node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+
+import axios from 'axios'
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
+
+import {
+  Container,
+  Image,
+  ContainerItens,
+  H1,
+  InputLabel,
+  Input,
+  Button,
+  User
+} from "./styles";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
+
+
+  async function addNewUser() {
+    const { data: newUser } = await axios.post("http://localhost:3001/users", {
+      name: inputName.current.value, 
+      age: inputAge.current.value,
+     });
+
+     setUsers([ ...users, newUser]);
+ 
+  }
+
+  useEffect(() => {
+    async function fetchUsers(){
+    
+    
+      const { data: newUsers } = await axios.get ("http://localhost:3001/users");
+
+      setUsers(newUsers);
+    }
+
+    fetchUsers()
+  },[])
+
+function deleteUser(userId){
+    const newUsers = users.filter( user => user.id !== userId)
+    setUsers(newUsers)
 }
 
-export default App;
+return (
+  <Container>
+    <Image alt="logo-imagem" src={People} />
+    <ContainerItens>
+      <H1>Olá!</H1>
+
+      <InputLabel>Nome</InputLabel>
+      <Input ref={inputName} placeholder="Nome" />
+
+      <InputLabel> Idade</InputLabel>
+      <Input ref={inputAge} placeholder="Idade" />
+
+      <Button onClick={addNewUser}>
+        Cadastrar <img alt="seta" src={Arrow} />
+      </Button>
+
+      <ul>
+        {users.map((user) => (
+          <User key={user.id}>
+            <p> {user.name} </p> <p>{user.age}</p>
+            <button onClick={()=> deleteUser(user.id)}>
+              <img src={Trash} alt="lata-de-lixo" /></button>
+          </User>
+
+        ))}
+      </ul>
+
+
+    </ContainerItens>
+  </Container>
+);
+}
+
+export default App
